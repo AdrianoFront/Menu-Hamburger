0bc9f400d8753aba854893e23b8377947960949b
Adriano <98757084+AdrianoFront@users.noreply.github.com>
2022-05-23 19:25:01 -0300

    open menu in js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 let menuMobile = document.querySelector(".menuMobile");
+let botaoMenu = document.querySelector(".botaoMenu");
+
+let aberto = false;
+
+function abrirOufecharMenu() {
+  if (aberto === true) {
+    aberto = false;
+    menuMobile.classList.remove("aberto");
+    botaoMenu.innerText = "abrir menu";
+  } else if (aberto === false) {
+    aberto = true;
+    menuMobile.classList.add("aberto");
+    botaoMenu.innerText = "fechar menu";
+  }
+}
